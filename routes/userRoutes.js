730fc6a647a0b10eddda7d5d711def95db296bd5
Sanjeev Kumar Jha <mr.sanjeevjha07@gmail.com>
2025-08-14 19:53:14 +0530

    Updated userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,59 +1,28 @@
 import express from "express";
 import {
-  getUserProfile,
+  dropUser,
+  getUserProfileController,
   loginController,
   logoutController,
-  passwordResetController,
   registerController,
-  updatePasswordControll,
-  updateProfileController,
-  updateProfilePhotoController,
+  updatePassController,
+  updatePicController,
 } from "../controller/userController.js";
-import { isAuth } from "../middlewares/authMiddleware.js";
+import { isAdmin, isAuth } from "../middlewares/authMiddleware.js";
 import { singleUpload } from "../middlewares/multer.js";
-import { rateLimit } from "express-rate-limit";
 
-// RATE LIMITER
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 min
-  limit: 100, // Limit each IP to 100 requests per 'window' (here, per 15 mins).
-  standardHeaders: "draft-7", // draft-6: 'RateLimit-*' headers; draft-7:combined 'RateLimit' header
-  legacyHeaders: false, // Disable the 'X-RateLimit-* headers,
-  // store: ..., // use an exteral store for consistency across multiple server instances.
-});
-
-//router object
+// router object
 const router = express.Router();
 
-//routes
-//for register
-router.post("/register", limiter, registerController);
+// routes
+router.post("/register", registerController);
+router.post("/login", loginController);
+router.get("/logout", logoutController);
+router.get("/profile", isAuth, getUserProfileController);
+router.put("/update-password", isAuth, updatePassController);
+router.delete("/drop-user/:id", isAuth, isAdmin, dropUser);
 
-//for login
-router.post("/login", limiter, loginController);
+router.put("/update-pic", isAuth, singleUpload, updatePicController);
 
-//profile
-router.get("/profile", isAuth, getUserProfile);
-
-//logout
-router.get("/logout", isAuth, logoutController);
-
-//update profile
-router.put("/profile-update", isAuth, updateProfileController);
-
-//update password
-router.put("/update-password", isAuth, updatePasswordControll);
-
-//update profile pic
-router.put(
-  "/update-picture",
-  isAuth,
-  singleUpload,
-  updateProfilePhotoController
-);
-
-// forgot password
-router.post("/reset-password", passwordResetController);
-
-//export
+// export router
 export default router;
